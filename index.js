944df7bf67a1222b77cc413c44a93a88c6949a94
Robragedu12 <robin.vaysse12000@gmail.com>
2026-09-10 12:11:16 +0200

    #feat ajout de fonctionalités dans index.js

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ import _ from "lodash";
 
 console.log(_.upperCase("bonjour npm"));
 console.log(dayjs().format("DD/MM/YYYY"));
+console.log(dayjs().unix());
+console.log(_.isNil(null) ? "vrai" : "faux");
+console.log(_.toFinite(Infinity));
