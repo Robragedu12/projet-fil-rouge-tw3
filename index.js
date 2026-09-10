52f1a5b24e3c6be879ea798b87206b36292b80e1
Robragedu12 <robin.vaysse12000@gmail.com>
2026-09-10 12:27:22 +0200

    Ajout de husky

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // index.js
-import dayjs from "dayjs";
-import _ from "lodash";
+import dayjs from 'dayjs';
+import _ from 'lodash';
 
-console.log(_.upperCase("bonjour npm"));
-console.log(dayjs().format("DD/MM/YYYY"));
+console.log(_.upperCase('bonjour npm'));
+console.log(dayjs().format('DD/MM/YYYY'));
 console.log(dayjs().unix());
-console.log(_.isNil(null) ? "vrai" : "faux");
+console.log(_.isNil(null) ? 'vrai' : 'faux');
 console.log(_.toFinite(Infinity));
